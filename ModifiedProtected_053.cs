43bca1d9a44be630b121dda14a916ecdcc532ec6
JaTri19 <115677162+JaTri19@users.noreply.github.com>
2022-10-13 10:07:23 -0700

    Add files via upload

---
+++ ModifiedProtected_053.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ModifiedProtected
+{
+
+        class Person
+        {
+            protected string name = "Jarwo Trimulyo";
+        }
+
+        // derived class
+        class Program : Person
+        {
+            static void Main(string[] args)
+            {
+
+                // creating object of derived class
+                Program program = new Program();
+
+                // accessing name field and printing it
+                Console.WriteLine("Name: " + program.name);
+                Console.ReadLine();
+            }
+        }
+}
+
