dd36f9be16e9d6c5ce910c9db9d7f1b2a6098558
JaTri19 <115677162+JaTri19@users.noreply.github.com>
2022-10-13 09:58:33 -0700

    Add files via upload

---
+++ ModifiedPrivate_053.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ModifiedPrivate
+{
+    class Program
+    {
+        class Person
+        {
+            private string name = "Jarwo Trimulyo";
+            static void Main(string[] args)
+            {
+                Person person = new Person();
+                Console.WriteLine(person.name);
+            }
+        }
+    }
+}
