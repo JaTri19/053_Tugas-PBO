b30dd7c02810a53a7cdfc5045c25de0f8227f7df
JaTri19 <115677162+JaTri19@users.noreply.github.com>
2022-10-13 10:11:06 -0700

    Add files via upload

---
+++ Constructor_053.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Constructor
+{
+    public class Program
+    {
+        public class User
+        {
+            public string username;
+            public string password;
+
+            public User(string username, string password)
+            {
+                this.username = username;
+                this.password = password;
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            User petani = new User("Jarwo Trimulyo", "21051214053");
+            Console.WriteLine("Username: " + petani.username);
+            Console.WriteLine("Password: " + petani.password);
+            Console.ReadLine();
+        }
+    }
+}
